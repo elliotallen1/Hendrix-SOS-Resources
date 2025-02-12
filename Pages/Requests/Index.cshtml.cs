c2bdb870418824b2e0d611cd0991f4eb04a29ef0
elliotallen1 <allened@hendrix.edu>
2025-02-12 13:06:29 -0600

    Added Approve/Deny functionality
    Added buttons next to the request to approve or deny them. Until a button is clicked, the request will be "Pending"

--- Pages/Requests/Index.cshtml.cs
+++ Pages/Requests/Index.cshtml.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
 using HendrixSOSResources.Data;
 using SOSResources.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace HendrixSOSResources.Pages.Requests
 {
@@ -25,6 +26,36 @@ namespace HendrixSOSResources.Pages.Requests
             Requests = await _context.Requests
                 .Include(r => r.Resource)
                 .ToListAsync();
+        }
+
+        public async Task<IActionResult> OnPostApproveAsync(int id)
+        {
+            var request = await _context.Requests.FindAsync(id);
+            if (request == null)
+            {
+                return NotFound();
+            }
+
+            request.Status = RequestStatus.Approved;
+            _context.Update(request);
+            await _context.SaveChangesAsync();
+
+            return RedirectToPage();
+        }
+
+        public async Task<IActionResult> OnPostDenyAsync(int id)
+        {
+            var request = await _context.Requests.FindAsync(id);
+            if (request == null)
+            {
+                return NotFound();
+            }
+
+            request.Status = RequestStatus.Denied;
+            _context.Update(request);
+            await _context.SaveChangesAsync();
+
+            return RedirectToPage();
         }
     }
 }
